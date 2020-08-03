2eb2002928ce8a94c0525a577f792c12c3ac73c8
Kupcor4000 <piotr.kupczyk@best.krakow.pl>
2020-08-03 13:52:02 +0000

    dodanie osobnej funkcji weryfikacyjnej

--- linear_search.py
+++ linear_search.py
@@ -2,14 +2,18 @@ def linear_search(list_of_arguments,target):
     step = 1
     for i in range(len(list_of_arguments)):
         if int(list_of_arguments[i]) == target:
-            print("We find target number {} in {} atemps!".format(target,step))
             return i
         step += 1
-    print("Nie ma takiej liczby w zbiorze!")
     return None
-        
+
+def verify(index):
+    if index is not None:
+        print("Target found at index {}".format(index))
+    else:
+        print("Target not found!")
         
 user_list = input("Please enter some number: ")
-a_list = user_list.split();
-print(a_list)
-linear_search(a_list,5)
+a_list = user_list.split()
+
+iterator = linear_search(a_list,5)
+verify(iterator)
