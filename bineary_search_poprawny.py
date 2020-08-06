ec4ad748eac9dd0be7da355e11dfbde582d62264
Kupcor4000 <piotr.kupczyk@best.krakow.pl>
2020-08-06 17:11:25 +0000

    Dodalem mozliwosc wprowadzania listy przez uzywtkownika, lista jest sortowana za pomoca booble sorta zanim zostanie poddana szukaniu

--- bineary_search_poprawny.py
+++ bineary_search_poprawny.py
@@ -1,3 +1,5 @@
+#lista musi byc posortowana!
+
 def binary_search(list, target):
     first = 0
     last = len(list) - 1
@@ -7,9 +9,9 @@ def binary_search(list, target):
         midpoint = (first + last) //2 #zaokraglanie w dol
         step = step+1
         print("Przejscie: {}, midpoint: {} , aktualna wartosc na midpoincie: {}, first: {}, last: {}".format(step,midpoint,list[midpoint],first,last))
-        if list[midpoint] == target:
+        if int(list[midpoint]) == target:
             return midpoint
-        elif list[midpoint] < target:
+        elif int(list[midpoint]) < target:
             first = midpoint + 1 #wszystkie wartosci przed midpointem zostaja odrzucone
         #warunek kiedy target < list[midpoint]
         else:
@@ -23,7 +25,27 @@ def verify(wartosc):
     else:
         print("Szukana liczba nie znajduje sie w zbiorze")
 
+#booble sort
+#TODO dodać funkcje która sortuje liste podaną przez uzytkownika
+def sorting(a_list):
+        i = True
+        while i:
+            z = True
+            for j in range(len(a_list)-1):
+                if int(a_list[j]) > int(a_list[j+1]):
+                        k = a_list[j]
+                        a_list[j] = a_list[j+1]
+                        a_list[j+1] = k
+                        z = False
+            if z:
+                i = False
+        return a_list
+                   
         
-lista = [0,1,2,3,4,5,6,7,8]
-wynik = binary_search(lista,9)
+        
+lista = input("Please enter some number seperate by coma: ")
+lista = lista.split(" ")
+print("Posortowana lista: {}".format(sorting(lista)))
+ta = int(input("Ktora z podanych wyzej liczb chcesz znaleźć? "))
+wynik = binary_search(sorting(lista),ta)
 verify(wynik)
