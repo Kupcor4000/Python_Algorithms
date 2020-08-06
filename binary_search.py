041cf17aebd615a5411d71e4fe5bf9d846d7ece5
Kupcor4000 <piotr.kupczyk@best.krakow.pl>
2020-08-06 15:23:42 +0000

    zmiana petli for na while

--- binary_search.py
+++ binary_search.py
@@ -7,18 +7,21 @@ def binary_search(list_of_arguments,target):
     ogr_2 = len(list_of_arguments)
     index = int(round((ogr_2+ogr_1)/2))
     step = 1;
-    for x in range(len(list_of_arguments)):
+    while True:
+        print("ogr_1: {} ogr_2: {}".format(ogr_1,ogr_2))
         print("Przejsc: {}, aktualny index: {}, na ktorym jest wartosc: {}".format(step,index-1,list_of_arguments[index-1]))
         if int(list_of_arguments[index-1]) == target:
             return index - 1
         elif int(list_of_arguments[index-1]) > target:
-            ogr_2 = index;
+            ogr_2 = index
             index = int(round((ogr_2+ogr_1)/2))
         elif int(list_of_arguments[index-1])< target:
             ogr_1 = index
             index = int(round((ogr_2+ogr_1)/2))
+        elif index == len(list_of_arguments) or index == -1:
+            return None
         step += 1
-    return None
+        
 
 def verify(wartosc):
     if wartosc is not None:
@@ -27,7 +30,7 @@ def verify(wartosc):
         print("Szukana liczba nie znajduje sie w zborze")
         
 lista = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40]
-wynik = binary_search(lista,40)
+wynik = binary_search(lista,3)
 verify(wynik)
 
     
